c1e7f329581439ff35e24bcab906b816d9d5c2a2
Andrew <afandeandrew639@gmail.com>
2025-04-10 11:40:09 +0300

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,37 +1,76 @@
 import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css"; // Make sure you create and import this CSS file
+
 const Navbar = () => {
-    // const user= LocalStorage.getItem("user");
-    const user=JSON.parse(LocalStorage.getItem("user"));
-    const navigate =useNavigate();
-    const handleLogout =() => {
-        localStorage.clear();
-        navigate ("/sigin")
-};
-    return ( 
-        <section className="row">
-        <div className="col-md-12">
-            <div className="navbar navbar-expand-md navbar-light bg-light">
-                <Link to="/" className="navbar-brand"><img src="images/Verizon.png" alt="" height="50px"/></Link >
-                <button className="navbar-toggler" data-bs-target="#ivy" data-bs-toggle="collapse">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  const user = JSON.parse(localStorage.getItem("user"));
+  const navigate = useNavigate();
 
-                <div className="collapse navbar-collapse" id="ivy">
-                    <div className="navbar-nav">
-                        <Link to="/" className="nav-link"> <img src="images/images (1).jpeg" alt="" height="20px"/>Home </Link >
-                        <Link to="/addproducts" className="nav-link">Add Products</Link >
-                        
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/signin");
+  };
 
-                    </div>
-                    <div className="navbar-nav ms-auto">
-                        <Link to="/signin" className="nav-link"> <img src="images/login1.jpeg" alt="" height="50px"/> </Link >
-                        <Link to="/signup" className="nav-link"> <img src="images/signup2.jpeg" alt="" height="50px"/> </Link >
-                    </div>
-                </div>
+  return (
+    <section className="row">
+      <div className="col-md-12">
+        <div className="navbar navbar-expand-md glass-navbar">
+          <Link to="/" className="navbar-brand">
+            <img src="images/Verizon.png" alt="Logo" height="50px" />
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            data-bs-target="#ivy"
+            data-bs-toggle="collapse"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="ivy">
+            <div className="navbar-nav">
+              <Link to="/" className="nav-link text-white">
+                <img src="images/images (1).jpeg" alt="" height="20px" /> Home
+              </Link>
+              <Link to="/addproducts" className="nav-link text-white">
+                Add Products
+              </Link>
             </div>
+
+            <div className="navbar-nav ms-auto">
+              {user ? (
+                <>
+                  <b className="nav-link text-success">Hello {user.username}</b>
+                  <button
+                    className="nav-link btn btn-link text-white"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Link to="/signin" className="nav-link">
+                    <img
+                      src="images/login1.jpeg"
+                      alt="Login"
+                      height="50px"
+                    />
+                  </Link>
+                  <Link to="/signup" className="nav-link">
+                    <img
+                      src="images/signup2.jpeg"
+                      alt="Signup"
+                      height="50px"
+                    />
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
         </div>
+      </div>
     </section>
-     );
-}
- 
+  );
+};
+
 export default Navbar;
